ad1eecf289bd8507355484b9e36bae21a99f273a
dgeene <david.geene@gmail.com>
2025-02-13 20:00:34 -0500

    finish starter file. program compiles and runs on ubuntu 24

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 
 Let's program Doom
 https://www.youtube.com/watch?v=huMO4VQEwPc
+
+OpenGL starter code
+https://github.com/3DSage/OpenGL-Starter_v1
+
+
+## Ubuntu
+To compile on Ubuntu 24.04
+
+Install libglut-dev
+
+Include directory is /usr/include/GL/
+
+To compile: `gcc main.c -lglut -lGL -lGLU`
+
+Helpful for getting compiling working
+https://stackoverflow.com/questions/9454398/qt-error-undefined-reference-to-gluortho2d
+
+
--- main.c
+++ main.c
@@ -40,6 +40,103 @@ void pixel(int x, int y, int c) { // draw a pixel at x/y with rgb
     glEnd();
 }
 
-int main() {
+void movePlayer() {
+    //move up, down, left, right
+    if(K.a ==1 && K.m==0){ printf("left\n");  }  
+    if(K.d ==1 && K.m==0){ printf("right\n"); }
+    if(K.w ==1 && K.m==0){ printf("up\n");    }
+    if(K.s ==1 && K.m==0){ printf("down\n");  }
+    //strafe left, right
+    if(K.sr==1){ printf("strafe left\n");     }
+    if(K.sl==1){ printf("strafe right\n");    }
+    //move up, down, look up, look down
+    if(K.a==1 && K.m==1){ printf("look up\n");   }
+    if(K.d==1 && K.m==1){ printf("look down\n"); }
+    if(K.w==1 && K.m==1){ printf("move up\n");   }
+    if(K.s==1 && K.m==1){ printf("move down\n"); }
+}
 
+void clearBackground() {
+    int x,y;
+    for(y=0;y<SH;y++) { //clear background color
+        for(x=0;x<SW;x++) {
+            pixel(x,y,8);
+        }
+    }
+}
+
+int tick;
+void draw3D() {
+    int x,y,c=0;
+    for(y=0;y<SH2;y++) {
+        for(x=0;x<SW2;x++) {
+            pixel(x,y,c);
+            c+=1;
+            if(c>8) {
+                c=0;
+            }
+        }
+    }
+    //frame rate
+    tick+=1;
+    if(tick>20) {
+        tick=0;
+    }
+    pixel(SW2,SH2+tick,0); 
+}
+
+void display() {
+    int x,y;
+    if(T.fr1-T.fr2>=50) {    //only draw 20 frames/second 
+        clearBackground();
+        movePlayer();
+        draw3D(); 
+
+        T.fr2=T.fr1;
+        glutSwapBuffers();
+        glutReshapeWindow(GLSW,GLSH); //prevent window scaling
+    }
+
+    T.fr1=glutGet(GLUT_ELAPSED_TIME);  //1000 Milliseconds per second
+    glutPostRedisplay();
+}
+
+void KeysDown(unsigned char key,int x,int y) { 
+    if(key=='w'==1){ K.w =1;} 
+    if(key=='s'==1){ K.s =1;} 
+    if(key=='a'==1){ K.a =1;} 
+    if(key=='d'==1){ K.d =1;} 
+    if(key=='m'==1){ K.m =1;} 
+    if(key==','==1){ K.sr=1;} 
+    if(key=='.'==1){ K.sl=1;} 
+}
+
+void KeysUp(unsigned char key,int x,int y) { 
+    if(key=='w'==1){ K.w =0;}
+    if(key=='s'==1){ K.s =0;}
+    if(key=='a'==1){ K.a =0;}
+    if(key=='d'==1){ K.d =0;}
+    if(key=='m'==1){ K.m =0;}
+    if(key==','==1){ K.sr=0;} 
+    if(key=='.'==1){ K.sl=0;}
+}
+
+void init() {       
+}
+
+
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowPosition(GLSW/2,GLSH/2);
+    glutInitWindowSize(GLSW,GLSH);
+    glutCreateWindow("");
+    glPointSize(pixelScale);      //pixel size
+    gluOrtho2D(0,GLSW,0,GLSH);    //origin bottom left
+    init();
+    glutDisplayFunc(display);
+    glutKeyboardFunc(KeysDown);
+    glutKeyboardUpFunc(KeysUp);
+    glutMainLoop();
+    return 0;
 }
